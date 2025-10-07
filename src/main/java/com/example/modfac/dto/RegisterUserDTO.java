58d3f2d936316d0739b78072c75f41d712edab5d
alechulkin <oleksandr.chulkin@yahoo.com>
2025-10-07 15:56:16 +0300

    refactoring-tool(C:\Users\oleksandr.chulkin\Documents\repo\modfac\src\main\java\com\example\modfac\dto\RegisterUserDTO.java): Please add the javadoc to class declaration and the methods in case javadoc is missing. Do not add any javadoc comments for methods and classes in case Javadoc is already present.

--- src/main/java/com/example/modfac/dto/RegisterUserDTO.java
+++ src/main/java/com/example/modfac/dto/RegisterUserDTO.java
@@ -6,12 +6,25 @@ import lombok.Data;
 
 @Data
 public class RegisterUserDTO {
-    @NotBlank
-    @Size(min = 3, max = 50)
-    private String username;
+    /**
+     * Data Transfer Object (DTO) for registering a new user.
+     * <p>
+     * This class is used to encapsulate the data required for user registration,
+     * including username and password. It includes validation constraints to ensure
+     * that the provided data meets the required criteria.
+     */
+    /**
+         * The username of the user to be registered.
+         * <p>
+         * This field must not be blank and must have a length between 3 and 50 characters.
+         */
+        private String username;
     
-    @NotBlank
-    @Size(min = 6, max = 40)
-    private String password;
+    /**
+         * The password of the user to be registered.
+         * <p>
+         * This field must not be blank and must have a length between 6 and 40 characters.
+         */
+        private String password;
 }
 
