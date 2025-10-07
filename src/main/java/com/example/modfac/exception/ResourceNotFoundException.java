295a9128c6dc281592db66c0bac0034fe07774d2
alechulkin <oleksandr.chulkin@yahoo.com>
2025-10-07 15:57:49 +0300

    refactoring-tool(C:\Users\oleksandr.chulkin\Documents\repo\modfac\src\main\java\com\example\modfac\exception\ResourceNotFoundException.java): Please add the javadoc to class declaration and the methods in case javadoc is missing. Do not add any javadoc comments for methods and classes in case Javadoc is already present.

--- src/main/java/com/example/modfac/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/modfac/exception/ResourceNotFoundException.java
@@ -1,6 +1,22 @@
 package com.example.modfac.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
+    /**
+     * Constructs a new ResourceNotFoundException with the specified detail message.
+     *
+     * @param message the detail message, which provides more information about the exception.
+     */
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    /**
+     * Exception thrown when a requested resource is not found.
+     * <p>
+     * This exception is a runtime exception and can be used to indicate
+     * that a specific resource, such as an entity or file, could not be located.
+     * </p>
+     */
     public ResourceNotFoundException(String message) {
         super(message);
     }
