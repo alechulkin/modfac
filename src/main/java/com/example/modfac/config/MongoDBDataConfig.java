1165561e53afb32144583a2d73134493cdedc5e2
alechulkin <oleksandr.chulkin@yahoo.com>
2025-10-07 15:52:21 +0300

    refactoring-tool(C:\Users\oleksandr.chulkin\Documents\repo\modfac\src\main\java\com\example\modfac\config\MongoDBDataConfig.java): Please add the javadoc to class declaration and the methods in case javadoc is missing. Do not add any javadoc comments for methods and classes in case Javadoc is already present.

--- src/main/java/com/example/modfac/config/MongoDBDataConfig.java
+++ src/main/java/com/example/modfac/config/MongoDBDataConfig.java
@@ -11,17 +11,32 @@ import org.springframework.context.annotation.DependsOn;
 @DependsOn("mongoDBIndexConfig")
 @RequiredArgsConstructor
 public class MongoDBDataConfig {
+    /**
+     * Configuration class for MongoDB data operations.
+     * <p>
+     * This class is responsible for managing the generation of data in MongoDB
+     * based on the application configuration. It uses the {@link DataService}
+     * to perform data generation tasks when the 'data.generate' property is set to true.
+     * </p>
+     */
 
     private final DataService dataService;
 
     @Value("${data.generate}")
     private Boolean generateData;
 
-    @PostConstruct
-    public void generateData() {
-        if (Boolean.TRUE.equals(generateData)) {
-            dataService.generateData();
+    /**
+         * Generates data in MongoDB if the 'data.generate' property is set to true.
+         * <p>
+         * This method is executed after the bean's properties have been set, and it
+         * delegates the data generation task to the {@link DataService}.
+         * </p>
+         */
+        @PostConstruct
+        public void generateData() {
+            if (Boolean.TRUE.equals(generateData)) {
+                dataService.generateData();
+            }
+    
         }
-
-    }
 }
