dd385a3ce880315d2b6f429b4ae6581a5dd6d753
alechulkin <oleksandr.chulkin@yahoo.com>
2025-10-07 16:03:05 +0300

    refactoring-tool(C:\Users\oleksandr.chulkin\Documents\repo\modfac\src\main\java\com\example\modfac\security\JwtAuthenticationFilter.java): Please add the javadoc to class declaration and the methods in case javadoc is missing. Do not add any javadoc comments for methods and classes in case Javadoc is already present.

--- src/main/java/com/example/modfac/exception/UnauthorizedException.java
+++ src/main/java/com/example/modfac/exception/UnauthorizedException.java
@@ -2,15 +2,6 @@ package com.example.modfac.exception;
 
 public class UnauthorizedException extends RuntimeException {
     /**
-         * Constructs a new UnauthorizedException with the specified detail message.
-         *
-         * @param message the detail message, which provides more information about the exception.
-         */
-        public UnauthorizedException(String message) {
-            super(message);
-        }
-
-    /**
      * This exception is thrown to indicate that a user is not authorized to perform the requested operation.
      * It extends the {@link RuntimeException} class, allowing it to be used as an unchecked exception.
      *
