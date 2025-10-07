297435566b69644cb0228980f4e8fc000933f7f9
alechulkin <oleksandr.chulkin@yahoo.com>
2025-10-07 15:53:43 +0300

    refactoring-tool(C:\Users\oleksandr.chulkin\Documents\repo\modfac\src\main\java\com\example\modfac\config\ObjectIdSerializer.java): Please add the javadoc to class declaration and the methods in case javadoc is missing. Do not add any javadoc comments for methods and classes in case Javadoc is already present.

--- src/main/java/com/example/modfac/config/ObjectIdSerializer.java
+++ src/main/java/com/example/modfac/config/ObjectIdSerializer.java
@@ -8,6 +8,25 @@ import org.bson.types.ObjectId;
 import java.io.IOException;
 
 public class ObjectIdSerializer extends StdSerializer<ObjectId> {
+    /**
+     * A custom serializer for the {@link ObjectId} class from the BSON library.
+     * <p>
+     * This serializer converts an {@link ObjectId} instance into its hexadecimal string representation
+     * when serializing JSON objects. It is particularly useful when working with MongoDB ObjectIds
+     * in a Java application that uses Jackson for JSON serialization.
+     * </p>
+     * <p>
+     * Usage:
+     * <pre>
+     * {@code
+     * ObjectMapper mapper = new ObjectMapper();
+     * SimpleModule module = new SimpleModule();
+     * module.addSerializer(ObjectId.class, new ObjectIdSerializer());
+     * mapper.registerModule(module);
+     * }
+     * </pre>
+     * </p>
+     */
     public ObjectIdSerializer() {
         this(null);
     }
@@ -16,9 +35,46 @@ public class ObjectIdSerializer extends StdSerializer<ObjectId> {
         super(t);
     }
 
+    /**
+     * Serializes an {@link ObjectId} instance into its hexadecimal string representation.
+     * <p>
+     * This method is invoked during the JSON serialization process to convert an {@link ObjectId}
+     * into a format that can be represented as a JSON string.
+     * </p>
+     *
+     * @param objectId the {@link ObjectId} instance to serialize.
+     * @param gen the {@link JsonGenerator} used to write JSON content.
+     * @param provider the {@link SerializerProvider} that can be used to get serializers for
+     *                 serializing objects value contains, if any.
+     * @throws IOException if an I/O error occurs during serialization.
+     */
     @Override
     public void serialize(ObjectId objectId, JsonGenerator gen, SerializerProvider provider) throws IOException {
         gen.writeString(objectId.toHexString());
+    }
+
+    /**
+     * Parameterized constructor for ObjectIdSerializer.
+     * <p>
+     * This constructor allows specifying a type to be used by the serializer.
+     * It is typically used when a custom serializer is required for a specific type of {@link ObjectId}.
+     * </p>
+     *
+     * @param t the class type to be used by the serializer, usually {@link ObjectId}.
+     */
+    public ObjectIdSerializer(Class<ObjectId> t) {
+        super(t);
+    }
+
+    /**
+     * Default constructor for ObjectIdSerializer.
+     * <p>
+     * This constructor initializes the serializer without a specific type, allowing it to handle
+     * serialization of {@link ObjectId} instances to their hexadecimal string representation.
+     * </p>
+     */
+    public ObjectIdSerializer() {
+        this(null);
     }
 }
 
