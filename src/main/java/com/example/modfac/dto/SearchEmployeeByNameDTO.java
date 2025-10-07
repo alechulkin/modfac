ae5c3d6f04622b3b3ef2deef7eed43e45603c1cf
alechulkin <oleksandr.chulkin@yahoo.com>
2025-10-07 16:04:32 +0300

    refactoring-tool(C:\Users\oleksandr.chulkin\Documents\repo\modfac\src\main\java\com\example\modfac\service\LeaveService.java): Please add the javadoc to class declaration and the methods in case javadoc is missing. Do not add any javadoc comments for methods and classes in case Javadoc is already present.

--- src/main/java/com/example/modfac/dto/SearchEmployeeByNameDTO.java
+++ src/main/java/com/example/modfac/dto/SearchEmployeeByNameDTO.java
@@ -14,32 +14,13 @@ public class SearchEmployeeByNameDTO {
      * to search for, as well as pagination parameters such as page number and page size.
      * </p>
      */
-    /**
-     * The name of the employee to search for.
-     * <p>
-     * This field is mandatory and must meet the following validation constraints:
-     * <ul>
-     *   <li>Cannot be blank.</li>
-     *   <li>Must be at least 3 characters long.</li>
-     * </ul>
-     * </p>
-     */
+    @NotBlank(message = "Search name is required")
+    @Size(min = 3, message = "Search term must be at least 2 characters")
+    private String name;
 
-    /**
-     * The page number for pagination.
-     * <p>
-     * This field determines the current page of the search results to retrieve.
-     * The default value is 0, which corresponds to the first page.
-     * </p>
-     */
+    private int page = 0;
 
-    /**
-     * The number of records per page for pagination.
-     * <p>
-     * This field determines how many records are displayed on each page of the search results.
-     * The default value is 10.
-     * </p>
-     */
+    private int size = 10;
 
 }
 
