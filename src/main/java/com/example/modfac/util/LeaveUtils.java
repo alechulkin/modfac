d6b9ee63146df3719dcf5a179b91508d07e63117
alechulkin <oleksandr.chulkin@yahoo.com>
2025-10-08 22:05:02 +0300

    refactoring-tool(C:\Users\oleksandr.chulkin\Documents\repo\modfac\src\main\java\com\example\modfac\util\LeaveUtils.java):  Add a static logger to the LeaveUtils class since it doesn't have one. Add debug-level logs to the getLeaveDays method. Add debug-level logs to the getRandomDate method. Add debug-level logs to the getRandomLeaveType method. Add debug-level logs to the getRandomStatus method. Add debug-level logs to the getRandomDays method.

--- src/main/java/com/example/modfac/util/LeaveUtils.java
+++ src/main/java/com/example/modfac/util/LeaveUtils.java
@@ -9,6 +9,7 @@ import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public final class LeaveUtils {
+    private static final org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger(LeaveUtils.class);
     public static final int LEAVE_PERIOD_DAYS = 365;
     public static final LocalDate LEAVE_PERIOD_STARTING_DATE = LocalDate.of(2025, 1, 1);
 
@@ -18,24 +19,39 @@ public final class LeaveUtils {
     }
 
     public static int getLeaveDays(LocalDate startDate, LocalDate endDate) {
-        return (int) ChronoUnit.DAYS.between(startDate, endDate) + 1;
+        LOGGER.debug("getLeaveDays method invoked");
+        int leaveDays = (int) ChronoUnit.DAYS.between(startDate, endDate) + 1;
+        LOGGER.debug("getLeaveDays method finished");
+        return leaveDays;
     }
 
     public static LocalDate getRandomDate(LocalDate startDate) {
+        LOGGER.debug("getRandomDate method invoked");
         long randomDays = getRandomDays();
-        return startDate.plusDays(randomDays);
+        LocalDate resultDate = startDate.plusDays(randomDays);
+        LOGGER.debug("getRandomDate method finished");
+        return resultDate;
     }
 
     public static LeaveType getRandomLeaveType() {
+        LOGGER.debug("getRandomLeaveType method invoked");
         LeaveType[] leaveTypes = LeaveType.values();
-        return leaveTypes[RANDOM.nextInt(leaveTypes.length)];
+        LeaveType result = leaveTypes[RANDOM.nextInt(leaveTypes.length)];
+        LOGGER.debug("getRandomLeaveType method finished");
+        return result;
     }
 
     public static Status getRandomStatus() {
-        return Status.values()[RANDOM.nextInt(Status.values().length)];
+        LOGGER.debug("getRandomStatus method invoked");
+        Status result = Status.values()[RANDOM.nextInt(Status.values().length)];
+        LOGGER.debug("getRandomStatus method finished");
+        return result;
     }
 
     private static long getRandomDays() {
-        return (long) (RANDOM.nextDouble() * LEAVE_PERIOD_DAYS);
+        LOGGER.debug("getRandomDays method invoked");
+        long randomDays = (long) (RANDOM.nextDouble() * LEAVE_PERIOD_DAYS);
+        LOGGER.debug("getRandomDays method finished");
+        return randomDays;
     }
 }
