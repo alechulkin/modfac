47700b9a57821b8a2a0583705b880f87b9c1250d
alechulkin <oleksandr.chulkin@yahoo.com>
2025-10-08 22:12:53 +0300

    refactoring-tool(C:\Users\oleksandr.chulkin\Documents\repo\modfac\src\main\java\com\example\modfac\ModfacApplication.java):  Add a logger to the ModfacApplication class since it does not currently have one. Add a debug log at the beginning of the main method to indicate its invocation. Add a debug log at the end of the main method to indicate its completion.

--- src/main/java/com/example/modfac/ModfacApplication.java
+++ src/main/java/com/example/modfac/ModfacApplication.java
@@ -9,9 +9,12 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @ComponentScan(basePackages = "com.example.modfac")
 @EnableMongoRepositories
 public class ModfacApplication {
+    private static org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger(ModfacApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(ModfacApplication.class, args);
-	}
+    		LOGGER.debug("main method invoked");
+    		SpringApplication.run(ModfacApplication.class, args);
+    		LOGGER.debug("main method finished");
+    	}
 
 }
